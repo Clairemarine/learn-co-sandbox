8fb46051f264561d82701b820195f69fa98100f1
Claire Nguyen <pandascn@gmail.com>
2018-08-01 23:04:25 +0000

    automatically backed up by learn

--- fbi/missions.rb
+++ fbi/missions.rb
@@ -1,10 +1,10 @@
 missions = ["Operation Addiction", "Operation Woof", "Operation Goner"]
 m=missions.sample
 
-success= ["Your banana peel saved the day and now you're the top agent in the FBI!", "Toby was a sneaky spy and quite intelligent despite not finishing 3rd grade, your team was amazing and you solved the case like a true FBI agent!", "The underage driver didn't follow the speed limit or any of the laws but you got from place to place quickly which helped you find the most clues, you're worthy of a badge!"]
+success= ["Your banana peel saved the day and tripped the criminal and now you're the top agent in the FBI! Moral of the story, cartoons are the best resource for crime stopping.", "Toby was a sneaky spy and quite intelligent despite not finishing 3rd grade, your team was amazing and you solved the case like a true FBI agent!", "The underage driver didn't follow the speed limit or any of the laws but you got from place to place quickly which helped you find the most clues, you're worthy of a badge!"]
 s=success.sample
 
 failure= ["Remember Toby? He was salty that you didn't pick him and he took your case and solved it in 3 seconds, he's a pro. Find a different career path.", "Your vehicle broke down after the engine fell out, why'd you choose that one?? You're NOT an FBI agent.", "Your weapon couldn't hurt a mosquito when you needed it, when you tried to use it on the suspected criminal, they just walked away. How could you even consider being an FBI agent? Bye." ]
 f=failure.sample
 
-puts s
+puts 
