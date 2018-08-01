8674b3e1e48aef99d0e01c7c60f64ee6c6f95321
Claire Nguyen <pandascn@gmail.com>
2018-08-01 23:03:09 +0000

    automatically backed up by learn

--- fbi/missions.rb
+++ fbi/missions.rb
@@ -1,7 +1,5 @@
-array = ["Operation Addiction", "Operation Woof", "Operation Goner"]
-array.each do |missions|
-  m=missions.sample
-end
+missions = ["Operation Addiction", "Operation Woof", "Operation Goner"]
+m=missions.sample
 
 success= ["Your banana peel saved the day and now you're the top agent in the FBI!", "Toby was a sneaky spy and quite intelligent despite not finishing 3rd grade, your team was amazing and you solved the case like a true FBI agent!", "The underage driver didn't follow the speed limit or any of the laws but you got from place to place quickly which helped you find the most clues, you're worthy of a badge!"]
 s=success.sample
