a05d870599d76f2d83a1bb788c9d5f4a60e7a4a0
Claire Nguyen <pandascn@gmail.com>
2018-08-01 22:59:50 +0000

    automatically backed up by learn

--- fbi/missions.rb
+++ fbi/missions.rb
@@ -6,4 +6,4 @@ end
 success= ["Your banana peel saved the day and now you're the top agent in the FBI!", "Toby was a sneaky spy and quite intelligent despite not finishing 3rd grade, your team was amazing and you solved the case like a true FBI agent!", "The underage driver didn't follow the speed limit or any of the laws but you got from place to place quickly which helped you find the most clues, you're worthy of a badge!"]
 s=success.sample
 
-failure= ["Remember Toby? He was salty that you didn't pick him and he took your case and solved it in 3 seconds, he's a pro. Find a different career path", "" ]
+failure= ["Remember Toby? He was salty that you didn't pick him and he took your case and solved it in 3 seconds, he's a pro. Find a different career path", "Your vehicle broke down after the engine fell out, why'd you choose that one?? You're NOT an FBI agent.", "" ]
