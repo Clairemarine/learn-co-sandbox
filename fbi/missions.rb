1e819cf20ba5d805607c580146a6da6196ac6920
Claire Nguyen <pandascn@gmail.com>
2018-08-01 22:56:49 +0000

    automatically backed up by learn

--- fbi/missions.rb
+++ fbi/missions.rb
@@ -3,7 +3,7 @@ array.each do |missions|
   m=missions.sample
 end
 
-success= ["Your banana peel saved the day and now you're the top agent in the FBI!", "Bubbles the goldfish was low maintenance but a sneaky spy, your team was amazing and you solved the case like a true FBI agent!", "The underage driver didn't follow the speed limit or any of the laws but you got from place to place quickly which helped you find the most clues, you're worthy of a badge!"]
+success= ["Your banana peel saved the day and now you're the top agent in the FBI!", "Toby was a sneaky spy and quite intelligent despite not finishing 3rd grade, your team was amazing and you solved the case like a true FBI agent!", "The underage driver didn't follow the speed limit or any of the laws but you got from place to place quickly which helped you find the most clues, you're worthy of a badge!"]
 s=success.sample
 
-failure= [""]
+failure= ["Remember Toby? He was salty that you didn't pick him and he took your case and solved it in 3 seconds, he's a pro. Find a different career path", "" ]
