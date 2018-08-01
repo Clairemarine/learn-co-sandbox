9e00ba66d2586da21095f01e12b518a3b4eee75e
Claire Nguyen <pandascn@gmail.com>
2018-08-01 22:54:56 +0000

    automatically backed up by learn

--- fbi/missions.rb
+++ fbi/missions.rb
@@ -6,3 +6,4 @@ end
 success= ["Your banana peel saved the day and now you're the top agent in the FBI!", "Bubbles the goldfish was low maintenance but a sneaky spy, your team was amazing and you solved the case like a true FBI agent!", "The underage driver didn't follow the speed limit or any of the laws but you got from place to place quickly which helped you find the most clues, you're worthy of a badge!"]
 s=success.sample
 
+failure= [""]
